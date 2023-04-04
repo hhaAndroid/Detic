1835e9e18b02d7b84a585e7f96e8e0c63818b017
huanghaian <huanghaian@sensetime.com>
2023-04-04 16:01:23 +0800

    update

--- create_ov_coco_json.py
+++ create_ov_coco_json.py
@@ -1,6 +1,6 @@
 import json
 
-annotations_path = '/home/PJLAB/huanghaian/dataset/coco/annotations/'
+annotations_path = 'dataset/coco/annotations/'
 save_path = 'datasets/coco-zero-shot/'
 
 with open(annotations_path + 'instances_train2017.json', 'r') as fin:
